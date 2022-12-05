77382711e56b15fb2ceeab955f950c186acfe0a8
DzenSheep <DzenSheep@yandex.ru>
2022-12-05 19:27:01 +0300

    Final

--- scripts/index.js
+++ scripts/index.js
@@ -26,4 +26,44 @@ function Voice () {
 alert(arrayRandElement(arr))};
 
 const aarrr = document.querySelector('.button');
-aarrr.addEventListener('click', Voice);
+const center = document.querySelector('.center');
+const text = document.querySelector('.center__picture_text');
+const pictureClose = document.querySelector('.center__picture_close');
+pictureClose.addEventListener('click', event =>{
+  center.classList.toggle ('center__active');
+})
+
+aarrr.addEventListener('click', event => {
+  center.classList.toggle ('center__active');
+  text.textContent = arrayRandElement(arr);
+});
+
+
+const centerCats = document.querySelector('.center__cats');//Выбрали попап, где будет котик
+const centerCat = document.querySelector('.center__cat');//Выбрали котика в попапе
+const catImage = document.querySelectorAll('.cat__image');//Выбрали все фото котиков на странице
+
+catImage.forEach((catImage) => catImage.addEventListener('click', event =>{
+  centerCats.classList.toggle('center__cats_active')
+  centerCat.src = catImage.src;
+}))
+
+const catClose = document.querySelector('.center__cat_close').addEventListener('click', event =>{
+  centerCats.classList.remove('center__cats_active');
+
+})
+
+const anchors = document.querySelectorAll('a[href*="#"]') //Делаем переход по любой ссылке плавным
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
